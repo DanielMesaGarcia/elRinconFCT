5a52a51ee4c9e434782888063ab01db100a27b77
rebekkarutm <rmdottir@gmail.com>
2024-03-05 13:23:54 +0000

    tailwind and pages folder added

---
+++ src/pages/frontPage/frontPage.js
@@ -0,0 +1,37 @@
+import calendar from "../images/calendar.png";
+import browse from "./images/browse.png";
+import add from "./images/add.png";
+import hangoutCard from "./images/hangout.png";
+import phys_act from "./images/phys_act-icon.png";
+import arts from "./images/arts-icon.png";
+import hangout from "./images/hangout-icon.png";
+import food from "./images/food-icon.png";
+import edu from "./images/edu-icon.png";
+import out_act from "./images/out_act-icon.png";
+import events from "./images/events-icon.png";
+
+export default function FrontPage() {
+  return (
+    <div className="w-390 h-844 bg-grey">
+      <h1 className="font-2xl">Hi Steve</h1>
+      <h2 className="font-base">What would you like to do?</h2>
+      <div className="flex">
+        <img src={phys_act} alt="physical activity" />
+        <img src={arts} alt="arts and crafts" />
+        <img src={hangout} alt="hangout" />
+        <img src={food} alt="food" />
+        <img src={edu} alt="education" />
+        <img src={out_act} alt="outdoor activities" />
+        <img src={events} alt="events" />
+      </div>
+      <div>
+        <img className="w-330 h-534 rounded-2xl" src={hangoutCard} alt="" />
+      </div>
+      <div className="flex bg-white">
+        <img src={calendar} alt="calendar" />
+        <img src={browse} alt="browse" />
+        <img src={add} alt="create event" />
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js, jsx, ts, tsx}",
+  ],
+  theme: {
+    fontSize : {
+      sm: '12px',
+      base: '16px',
+      xl: '20px',
+      '2xl': '28px',
+      '3xl': '56px',
+    },
+    colors: {
+      'white' : 'ffffff',
+      'grey' : '#f5f5f5',
+      'dark-grey' : '#373737',
+      'red' : '#ffc2c2',
+      'orange' : '#ffeac2',
+      'pink' : '#ffc2f5',
+      'blue' : '#c2fbff',
+      'purple' : '#e4c2ff',
+      'green' : '#e0ffc2',
+      'yellow' : '#f8efa6'
+    },
+    extend: {
+      width: {
+        '844' : '844px',
+        '669' : '669px',
+        '534' : '534px',
+        '390' : '390px',
+        '388' : '388px',
+        '350' : '350px',
+      },
+    },
+  },
+  plugins: [],
+}
