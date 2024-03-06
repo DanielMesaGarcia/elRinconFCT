850d1911e802fa951e8040a0bc90bb4cf86440b0
rebekkarutm <rmdottir@gmail.com>
2024-03-06 08:55:40 +0000

    frontpage in progress

--- src/pages/frontPage/frontPage.js
+++ src/pages/frontPage/frontPage.js
@@ -1,17 +1,23 @@
-
 export default function FrontPage() {
   return (
     <div className="w-390 h-844 bg-grey">
       <h1 className="font-2xl">Hi Steve</h1>
       <h2 className="font-base">What would you like to do?</h2>
-      <div className="flex">
-        <img src="/images/phys_act-icon.png" alt="physical activity" />
-        <img src="/images/arts-icon.png" alt="arts and crafts" />
-        <img src="/images/hangout-icon.png" alt="hangout" />
-        <img src="/images/food-icon.png" alt="food" />
-        <img src="/images/edu-icon.png" alt="education" />
-        <img src="/images/out_act-icon.png" alt="outdoor activities" />
-        <img src="/images/events-icon.png" alt="events" />
+      <div className="grid-cols-14">
+        <img src="/images/phys_act-icon.png" alt="" />
+        <p>Physical</p>
+        <img src="/images/arts-icon.png" alt="" />
+        <p>Arts and crafts</p>
+        <img src="/images/hangout-icon.png" alt="" />
+        <p>Hangout</p>
+        <img src="/images/food-icon.png" alt="" />
+        <p>Food</p>
+        <img src="/images/edu-icon.png" alt="" />
+        <p>Education</p>
+        <img src="/images/out_act-icon.png" alt="" />
+        <p>Outdoors</p>
+        <img src="/images/events-icon.png" alt="" />
+        <p>Events</p>
       </div>
       <div>
         <img className="w-330 h-534 rounded-2xl" src="/images/hangout.png" alt="" />
