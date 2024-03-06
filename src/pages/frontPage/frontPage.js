b48b18741cdf269c8028a4395cd95bff05b2fe72
Daniel Mesa <daniwlmesa12@gmail.com>
2024-03-06 08:34:42 +0000

    index fix and pictures

--- src/pages/frontPage/frontPage.js
+++ src/pages/frontPage/frontPage.js
@@ -1,14 +1,3 @@
-import calendar from "../images/calendar.png";
-import browse from "./images/browse.png";
-import add from "./images/add.png";
-import hangoutCard from "./images/hangout.png";
-import phys_act from "./images/phys_act-icon.png";
-import arts from "./images/arts-icon.png";
-import hangout from "./images/hangout-icon.png";
-import food from "./images/food-icon.png";
-import edu from "./images/edu-icon.png";
-import out_act from "./images/out_act-icon.png";
-import events from "./images/events-icon.png";
 
 export default function FrontPage() {
   return (
@@ -16,21 +5,21 @@ export default function FrontPage() {
       <h1 className="font-2xl">Hi Steve</h1>
       <h2 className="font-base">What would you like to do?</h2>
       <div className="flex">
-        <img src={phys_act} alt="physical activity" />
-        <img src={arts} alt="arts and crafts" />
-        <img src={hangout} alt="hangout" />
-        <img src={food} alt="food" />
-        <img src={edu} alt="education" />
-        <img src={out_act} alt="outdoor activities" />
-        <img src={events} alt="events" />
+        <img src="/images/phys_act-icon.png" alt="physical activity" />
+        <img src="/images/arts-icon.png" alt="arts and crafts" />
+        <img src="/images/hangout-icon.png" alt="hangout" />
+        <img src="/images/food-icon.png" alt="food" />
+        <img src="/images/edu-icon.png" alt="education" />
+        <img src="/images/out_act-icon.png" alt="outdoor activities" />
+        <img src="/images/events-icon.png" alt="events" />
       </div>
       <div>
-        <img className="w-330 h-534 rounded-2xl" src={hangoutCard} alt="" />
+        <img className="w-330 h-534 rounded-2xl" src="/images/hangout.png" alt="" />
       </div>
       <div className="flex bg-white">
-        <img src={calendar} alt="calendar" />
-        <img src={browse} alt="browse" />
-        <img src={add} alt="create event" />
+        <img src="/images/calendar.png" alt="calendar" />
+        <img src="/images/browse.png" alt="browse" />
+        <img src="/images/add.png" alt="create event" />
       </div>
     </div>
   );
