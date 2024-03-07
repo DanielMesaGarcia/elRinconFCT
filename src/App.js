23ad070d201cd4ac787ecefe8832cbcca6ee8646
Mohamed <mohasadio2002@gmail.com>
2024-03-07 09:14:09 +0000

    i did match and nomatch pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import 'tailwindcss/tailwind.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FrontPage from "./pages/frontPage/frontPage";
 import CreateActivityPage from "./pages/createPage/createPage";
+import 'antd/dist/reset.css';
+import MatchPage from './pages/matchPage/matchPage';
+import NomatchPage from './pages/matchPage/nomatchPage';
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
         <Route path="/" element={<FrontPage/>}/>
         <Route path="/home" element={<FrontPage/>}/>
         <Route path="/createPage" element={<CreateActivityPage/>}/>
+        <Route path="/matchPage" element={<MatchPage/>}/>
+        <Route path="/nomatchPage" element={<NomatchPage/>}/>
       </Routes>
     </BrowserRouter>
   );
