c273a3314e4e01cf5e4006276cc6e2beff9790bd
hckjensen <hckjensen96@gmail.com>
2024-03-07 14:52:22 +0100

    margin fixed on calendar

--- src/pages/calendarPage/calendarPage.jsx
+++ src/pages/calendarPage/calendarPage.jsx
@@ -39,24 +39,26 @@ const CalendarPage = () => {
 
     return (
         <>
-            <body className="bg-grey h-full">
-                <div className="absolute top-8 left-4">
-                    <Link to="/">
-                        <img src="images/home.png" alt="Home" />
-                    </Link>
-                </div>
-                <div className="flex justify-center items-center w-full ">
-                    <h2 className="text-3xl font-semibold mt-12 mb-10">Calendar</h2>
-                </div>
-                <div className="h-[700px] overflow-y-auto">
-                    {sortedActivities.map(([date, activities], index) => (
-                        <div key={index} className="mt-10 flex flex-col gap-5">
-                            <h3 className="ml-4 mb-1 font-semibold">{formatDate(date)}</h3>
-                            {activities.map((activity, index) => (
-                                <ActivityCard key={index} activity={activity} />
-                            ))}
-                        </div>
-                    ))}
+            <body className="bg-grey">
+                <div className="flex flex-col h-844">
+                    <div className="absolute top-8 left-4">
+                        <Link to="/">
+                            <img src="images/home.png" alt="Home" />
+                        </Link>
+                    </div>
+                    <div className="flex justify-center items-center w-full ">
+                        <h2 className="text-3xl font-semibold mt-12 mb-10">Calendar</h2>
+                    </div>
+                    <div className=" mt-20 overflow-y-auto">
+                        {sortedActivities.map(([date, activities], index) => (
+                            <div key={index} className="mt-30 flex flex-col gap-5">
+                                <h3 className="ml-4 mb-1 font-semibold">{formatDate(date)}</h3>
+                                {activities.map((activity, index) => (
+                                    <ActivityCard key={index} activity={activity} />
+                                ))}
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </body>
         </>
