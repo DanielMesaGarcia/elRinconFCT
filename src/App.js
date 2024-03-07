2a9f949f446d37146121344ce12af8b101cc7a63
rebekkarutm <rmdottir@gmail.com>
2024-03-07 12:07:22 +0000

    frontpage completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import 'tailwindcss/tailwind.css'
-<<<<<<< HEAD
 
-=======
 import { BrowserRouter, Route, Routes } from "react-router-dom";
->>>>>>> 63ed7be30aeb3f39ecb557f599c79b2dffd2b344
 import FrontPage from "./pages/frontPage/frontPage";
 import CreateActivityPage from "./pages/createPage/createPage";
 
