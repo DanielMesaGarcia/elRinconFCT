75b6fbcafa6d209f65815fe752158db0c8eefcc3
Mohamed <mohasadio2002@gmail.com>
2024-03-08 08:22:32 +0000

    Merge branch 'develop' of https://github.com/DanielMesaGarcia/elRinconFCT into develop

--- src/pages/categoriesPage.js
+++
@@ -1,9 +0,0 @@
-export default function CategoriesPage(){
-    return(
-        <button className="bg-red-500 hover:bg-red-600
-                            text-white font-semibold py-2 px-4 rounded 
-                            focus:outline-none focus:ring focus:ring-red-300">
-      Logout
-    </button>
-    );
-}
