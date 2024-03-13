ece0e9afed30e1bc91f4044016544eb523ac33b7
rebekkarutm <rmdottir@gmail.com>
2024-03-13 13:55:12 +0000

    details page

--- src/components/DetailsCardComponent/DetailsCardComponent.js
+++ src/components/DetailsCardComponent/DetailsCardComponent.js
@@ -1,6 +1,5 @@
 import React from "react";
-import out_act from "../../assets/images/background imgs/out_act.png";
-
+import out_act from "../../assets/images/backgroundImgs/out_act.png";
 
 const DetailsCardComponent = ({ activity }) =>{
 
@@ -8,17 +7,40 @@ const DetailsCardComponent = ({ activity }) =>{
     return(
         <>
          <div className="bg-white shadow-md rounded-lg p-6">
-           <h2 className="text-xl font-bold mb-2">{activity.name}</h2>
            <div
-                style={{ "background-image": `url(${out_act})` }}
-                className="h-388 bg-cover bg-center relative rounded-t-lg"
+                style={{ "background-image": url(${out_act}) }}
+                className="h-250 bg-cover bg-center relative rounded-t-lg"
             >
                 <p className="text-4xl font-gemunu font-extrabold  pl-4">{activity.name}</p>
           </div>
-          <p className="text-sm text-gray-500">Date {activity.date}</p>
-           <p className="text-gray-600 mb-4">{activity.time}</p>
-          
-           <p className="text-sm text-gray-500">Location: {activity.location}</p>
+          <div className="flex">
+          <div className="ml-20 mt-30 w-1/2 p-4">
+            {/* Activity Description */}
+            <div className="">
+              <p className="line-clamp-3 font-light text-10">Date</p>
+              <p className="font-medium text-20"> {activity.date}</p>
+            </div><br></br>
+            <div className="line-clamp-3 font-light text-10">
+              <p className="line-clamp-3">Time</p>
+              <p className="font-medium text-20"> {activity.time}</p>
+            </div><br></br>
+            <div className="line-clamp-3 font-light text-10">
+              <p className="line-clamp-3">Location</p>
+              <p className="font-medium text-20"> {activity.location}</p>
+            </div><br></br>
+            <div className="line-clamp-3 font-light text-10">
+              <p className="line-clamp-3">Experience level</p>
+              <p className="font-medium text-20"> {activity.experience_level}</p>
+            </div><br></br>
+          </div>
+          <div className="ml-20 mt-30 w-1/2 p-4">
+            <div className="line-clamp-3 font-light text-10">
+              <p className="line-clamp-3">Attendees</p>
+              <p className="font-medium text-20"> {activity.max_attendees}</p>
+            </div><br></br>
+          </div>
+
+          </div>
          </div>
         </>
     );
