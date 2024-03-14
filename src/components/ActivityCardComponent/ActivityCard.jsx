d5d75ea308fdfa3be9154df2b2b13cef862fb3d0
hckjensen <hckjensen96@gmail.com>
2024-03-14 13:35:20 +0100

    asd

--- src/components/ActivityCardComponent/ActivityCard.jsx
+++ src/components/ActivityCardComponent/ActivityCard.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../../services/supabaseClient";
+import hangout from "../../assets/images/backgroundImgs/hangout.png";
+import arts from "../../assets/images/backgroundImgs/arts.png";
+import food from "../../assets/images/backgroundImgs/food.png";
+import physical from "../../assets/images/backgroundImgs/phys_act.png";
+import edu from "../../assets/images/backgroundImgs/edu.png";
+import events from "../../assets/images/backgroundImgs/events.png";
+import outdoors from "../../assets/images/backgroundImgs/out_act.png";
 
 const ActivityCard = ({ activity }) => {
     const navigate = useNavigate();
@@ -17,34 +24,51 @@ const ActivityCard = ({ activity }) => {
         setIsVisible(false);
         const { data, error } = await supabase
             .from('userTable')
-            .update({signedEvents: updatedEvents})
+            .update({ signedEvents: updatedEvents })
             .eq('email', localStorage.getItem('currentUser'));
 
         if (error) {
             throw error;
         }
-        console.log('Supabase response:', data); 
+        console.log('Supabase response:', data);
     }
 
     if (!isVisible) {
         return null; // If the card is not visible, return null to render nothing
     }
 
+    const activityImages = {
+        "Arts": arts,
+        "Food": food,
+        "Physical": physical,
+        "Education": edu,
+        "Events": events,
+        "Outdoor": outdoors,
+        "Hangout": hangout
+
+    }
+
     return (
         <div className="flex flex-row rounded-2xl w-[90%] h-[70px] m-auto bg-white shadow-md relative" >
             <div className="block w-[17px] rounded-l-2xl bg-purple"></div>
-            <div className="m-auto ml-3 text-2xl" onClick={handleCardClick}>
+            <div className="m-auto ml-3 text-xl" onClick={handleCardClick}>
                 <div>
                     <h2>{activity.name}</h2>
                 </div>
-                <div className="flex flex-row gap-1 h-full ">
+                <div className="flex flex-row gap-1 h-full w-[300px] ">
                     <p className="mb-0">{activity.time}</p>
                     <p className="mb-0">@</p>
                     <p className="mb-0">{activity.location}</p>
                 </div>
             </div>
-            <div className="bg-green w-[92px] rounded-r-2xl">
-                <img src={activity.imageUrl} alt={activity.title} />
+            <div style={{
+
+                "background-image": `url(${activityImages[activity.type] || 'url-to-default-image'})`,
+                "background-size": "cover",
+                "background-repeat": "no-repeat",
+                "width": "80px",
+
+            }} className="bg-green rounded-r-2xl">
             </div>
             <div className="absolute top-0 right-0 p-2" onClick={handleDeleteClick}>
                 <span role="img" aria-label="delete" className="text-red-500 cursor-pointer">🗑️</span>
--- src/components/MatchedComponent/MatchedComponent.jsx
+++ src/components/MatchedComponent/MatchedComponent.jsx
@@ -1,10 +1,54 @@
 import React from "react";
+import viewAll from "../../assets/images/grey cat/view_all-icon.svg"
+import physical from "../../assets/images/grey cat/physical-grey.svg"
+import arts from "../../assets/images/grey cat/arts-grey.svg"
+import outdoors from "../../assets/images/grey cat/outdoors-grey.svg"
+import edu from "../../assets/images/grey cat/edu-grey.svg"
+import food from "../../assets/images/grey cat/food-grey.svg"
+import hangout from "../../assets/images/grey cat/hangout-grey.svg"
+import events from "../../assets/images/grey cat/events-grey.svg"
 
-const MatchedComponent = () => {
+const MatchedComponent = ({ activity }) => {
+    const activityImages = {
+        "Arts": arts,
+        "Food": food,
+        "Physical": physical,
+        "Education": edu,
+        "Events": events,
+        "Outdoor": outdoors,
+        "Hangout": hangout
+
+    }
 
     return (
         <div>
-            <h1 className="text-3xl pt-110 pb-60 flex flex-row justify-center">You've got a match!</h1>
+            {/* the whole screen */}
+            <div className="px-40">
+                {/* match text */}
+                <h1 className="text-3xl pt-110 pb-60 flex flex-row justify-center">
+                    You've got a match!
+                </h1>
+                {/* event card */}
+                <div
+                    style={{
+
+                        "background-image": `url(${activityImages[activity.type] || 'url-to-default-image'})`,
+
+
+                    }}
+                    className="h-388 bg-cover bg-center relative rounded-2xl"
+                >
+                    <p className="text-4xl font-gemunu font-extrabold  pl-4">{activity.name}</p>
+                </div>
+                {/* add to calendar btn */}
+                <div className="flex justify-center pt-40">
+                    <button className="bg-darkGrey text-white w-310 h-60 rounded-2xl">
+                        Cool!
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
+
+export default MatchedComponent;
